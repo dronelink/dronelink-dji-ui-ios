c8b162ea4e82f8f10b98fb24e2e980d5c097ff3a
Jim McAndrew <jim@dronelink.com>
2020-12-03 10:31:52 -0600

    - Implemented DJIDroneSessionManager as WidgetFactory - Added DJIDashboardViewController.onDroneSessionManagerAdded

--- DronelinkDJIUI/DJIDashboardViewController.swift
+++ DronelinkDJIUI/DJIDashboardViewController.swift
@@ -970,6 +970,8 @@ public class DJIDashboardViewController: UIViewController {
 extension DJIDashboardViewController: DronelinkDelegate {
     public func onRegistered(error: String?) {}
     
+    public func onDroneSessionManagerAdded(manager: DroneSessionManager) {}
+    
     public func onMissionLoaded(executor: MissionExecutor) {
         DispatchQueue.main.async {
             self.missionExecutor = executor
